6ceef285fc2c08b200c41240b37772592026fda3
Arley <arleyspereira@hotmail.com>
2023-06-17 13:15:58 -0300

    atividades addEventListener

--- fundamentos/secao-04-dom-eventos-e-webstorage/dia-03-eventos/main.js
+++ fundamentos/secao-04-dom-eventos-e-webstorage/dia-03-eventos/main.js
@@ -20,30 +20,23 @@ for (let caixa of caixas) {
   });
 };
 
-function trocarTexto () {
+input.addEventListener("input", () => {
   const textoCaixa = document.querySelector('.tech');
   textoCaixa.innerHTML = input.value;
-};
+});
+function trocarCor () {
+  myWebpage.style.color = 'green';
+}
+function voltaCor () {
+  myWebpage.style.color = 'white';
+}
+myWebpage.addEventListener("dblclick", (event) => {
+  window.open("https://www.mozilla.org/", 'Mozila Webpage')
 
-input.addElementListener("change", trocarTexto);
+});
+myWebpage.addEventListener("mouseover", trocarCor);
+myWebpage.addEventListener("mouseout", voltaCor);
 
-
-
-// 1. Copie esse arquivo e edite apenas ele.
-// 1.1. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
-
-// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
-// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
-
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech'.
-
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
-// redirecione para alguma página.
-// 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
-
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo.
 
 // Segue abaixo um exemplo de uso do event.target.
 
